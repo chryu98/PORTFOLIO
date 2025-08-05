d48afb0d617c261ad6046926d7b4efaee6433cbf
fhqhzkq99 <fhqhzkq99@gmail.com>
2025-08-05 10:23:03 +0900

    1

--- BNK_spring/src/main/resources/static/js/header2.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-      header.classList.add('fixed');
-    } else {
-      header.classList.remove('fixed');
-    }
-  });
