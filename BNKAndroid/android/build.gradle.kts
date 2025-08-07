3ca640f0d84438dfa8551ace1a042bf34bcda5e4
SinSuwol <68939992+SinSuwol@users.noreply.github.com>
2025-08-07 14:10:49 +0900

    aa

--- BNKAndroid/android/build.gradle.kts
+++ BNKAndroid/android/build.gradle.kts
@@ -1,3 +1,13 @@
+// 🔧 루트 build.gradle.kts
+
+// 플러그인 정의
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+// 리포지토리 정의
 allprojects {
     repositories {
         google()
@@ -6,17 +16,17 @@ allprojects {
     }
 }
 
+// 💡 빌드 디렉토리 변경
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// 💣 태스크 직접 선언
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
