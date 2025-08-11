2d585690a351dd69f68a35466cf32fa872cec042
SinSuwol <68939992+SinSuwol@users.noreply.github.com>
2025-08-11 17:55:41 +0900

    dd

--- BNKAndroid/android/app/build.gradle.kts
+++ BNKAndroid/android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // Kotlin 안 쓸 거면 아래 줄 제거 유지
+    // id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -14,7 +14,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions { jvmTarget = "11" }
+    // ❌ kotlinOptions 블록 없음 (지워야 함)
 
     defaultConfig {
         applicationId = "com.example.bnkandroid"
@@ -31,6 +31,12 @@ android {
     }
 }
 
+dependencies {
+    implementation("com.naver.maps:map-sdk:3.22.1")
+}
+
 flutter {
     source = "../.."
 }
+
+
