b508b642854dbe324a6b18c1f1d2025fbc64e67f
SinSuwol <68939992+SinSuwol@users.noreply.github.com>
2025-08-11 10:23:12 +0900

    123

--- BNKAndroid/android/app/build.gradle.kts
+++ BNKAndroid/android/app/build.gradle.kts
@@ -1,28 +1,22 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")   // ← "kotlin-android" 말고 이걸 사용
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.bnkandroid"
     compileSdk = 36
-        ndkVersion = "27.0.12077973"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+    kotlinOptions { jvmTarget = "11" }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.bnkandroid"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -31,8 +25,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- BNKAndroid/android/build.gradle.kts
+++ BNKAndroid/android/build.gradle.kts
@@ -1,32 +1,13 @@
-// 🔧 루트 build.gradle.kts
+// android/build.gradle.kts  (루트)
 
-// 플러그인 정의
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-// 리포지토리 정의
 allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://naver.jfrog.io/artifactory/maven/") }
+        maven { url = uri("https://naver.jfrog.io/artifactory/maven/") } // 필요한 경우만 유지
     }
 }
 
-// 💡 빌드 디렉토리 변경
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-    project.evaluationDependsOn(":app")
-}
-
-// 💣 태스크 직접 선언
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    delete(rootProject.buildDir)
 }
