ff35d691caa9e563947bf302a88960aa28f5f1fd
SinSuwol <68939992+SinSuwol@users.noreply.github.com>
2025-08-13 09:05:18 +0900

    ㅇㅇㅇ

--- BNKAndroid/android/settings.gradle.kts
+++ BNKAndroid/android/settings.gradle.kts
@@ -1,3 +1,5 @@
+// settings.gradle.kts (Kotlin DSL)
+
 pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
@@ -12,7 +14,24 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
+        // Flutter Gradle 플러그인/임베딩 쪽 추가
+        maven(url = "https://storage.googleapis.com/download.flutter.io")
+        // 네이버맵 SDK 저장소 추가
+        maven(url = "https://naver.jfrog.io/artifactory/maven/")
         gradlePluginPortal()
+    }
+}
+
+// ★ 여기가 핵심: 실제 의존성 해석에 사용되는 저장소를 명시
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        google()
+        mavenCentral()
+        // Flutter 임베딩 아티팩트 저장소
+        maven(url = "https://storage.googleapis.com/download.flutter.io")
+        // 네이버맵 SDK 저장소
+        maven(url = "https://naver.jfrog.io/artifactory/maven/")
     }
 }
 
--- BNKAndroid/settings.gradle.kts
+++ BNKAndroid/settings.gradle.kts
@@ -3,7 +3,21 @@ pluginManagement {
         google()
         gradlePluginPortal()
         mavenCentral()
+        // 추가
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
+        maven { url = uri("https://naver.jfrog.io/artifactory/maven/") }
     }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
+        maven { url = uri("https://naver.jfrog.io/artifactory/maven/") }
+    }
+}
+
 rootProject.name = "bnkandroid"
 include(":app")
