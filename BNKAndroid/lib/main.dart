4334d5fd21a6e4ca6b18336f5451133294075974
fhqhzkq99 <fhqhzkq99@gmail.com>
2025-08-14 17:21:14 +0900

    지독한 지도 오류 해결

--- BNKAndroid/lib/main.dart
+++ BNKAndroid/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
 
-      home: LoginPage(),
+      home: NaverMapPage(),
 
 
 
